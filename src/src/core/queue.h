921a8e8d0b5a253a607c100f56980722a99de4bd
Shreyan Wasti <shreyanwasti@192.168.1.3>
2025-02-16 18:17:56 +0545

    queue struct improved

--- src/src/core/queue.h
+++ src/src/core/queue.h
@@ -1,16 +1,61 @@
-#include <SDL3/SDL.h>
-#include <vector>
+// src/core/queue.h
+#ifndef QUEUE_H
+#define QUEUE_H
 
-template <typename T> class Queue {
+template <typename T>
+class Queue {
 private:
-  std::vector<T> elements;
+    struct Node {
+        T data;
+        Node* next;
+        Node(const T& value) : data(value), next(nullptr) {}
+    };
+
+    Node* front;
+    Node* rear;
+    int size;
 
 public:
-  void enqueue(const T &value);
+    Queue() : front(nullptr), rear(nullptr), size(0) {}
+    ~Queue() {
+        while (!empty()) {
+            dequeue();
+        }
+    }
 
-  bool empty();
+    void enqueue(const T& value) {
+        Node* newNode = new Node(value);
+        if (empty()) {
+            front = rear = newNode;
+        } else {
+            rear->next = newNode;
+            rear = newNode;
+        }
+        size++;
+    }
 
-  void dequeue();
-  const T &front() const;
-  void printQueue() const;
+    void dequeue() {
+        if (empty()) return;
+
+        Node* temp = front;
+        front = front->next;
+        delete temp;
+        size--;
+
+        if (empty()) {
+            rear = nullptr;
+        }
+    }
+
+    T& peek() {
+        if (empty()) {
+            throw std::runtime_error("Queue is empty");
+        }
+        return front->data;
+    }
+
+    bool empty() const { return front == nullptr; }
+    int getSize() const { return size; }
 };
+
+#endif // QUEUE_H
